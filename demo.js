ffec982cceebdba97c9af3a879dceb9a0639dfb8
Sam Johnson <x.smashmaster.x@gmail.com>
2016-08-29 18:46:21 -0600

    Added cubemap reflections to blob

--- demo.js
+++ demo.js
@@ -3,10 +3,10 @@
 /* License: https://github.com/SmashMaster/smashmaster.github.io/blob/master/LICENSE */
 
 /* Browser update prompt: https://browser-update.org/ */
-var $buoop = {c:2}; 
-function $buo_f(){ 
- var e = document.createElement("script"); 
- e.src = "//browser-update.org/update.min.js"; 
+var $buoop = {c:2};
+function $buo_f(){
+ var e = document.createElement("script");
+ e.src = "//browser-update.org/update.min.js";
  document.body.appendChild(e);
 };
 try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
@@ -19,20 +19,20 @@ catch(e){window.attachEvent("onload", $buo_f)}
     var vendors = ['ms', 'moz', 'webkit', 'o'];
     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
                                    || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
- 
+
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
               timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
+
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
@@ -54,6 +54,7 @@ var canvas;
 var width, height;
 var aspectRatio;
 var gl;
+var texture;
 var shader, fsqBuffer;
 
 function onResize() {
@@ -69,9 +70,37 @@ function onResize() {
     }
 }
 
+function loadFace(target, url) {
+    var image = new Image();
+    $(image).one('load', function() {
+        gl.texImage2D(target, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    });
+    image.src = url;
+
+    return texture;
+}
+
+function makeCubemap() {
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
+
+    loadFace(gl.TEXTURE_CUBE_MAP_POSITIVE_X, "/cubemap/posx.png");
+    loadFace(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, "/cubemap/negx.png");
+    loadFace(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, "/cubemap/posy.png");
+    loadFace(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, "/cubemap/negy.png");
+    loadFace(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, "/cubemap/posz.png");
+    loadFace(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, "/cubemap/negz.png");
+}
+
 function reqSource(url) {
     var req = new XMLHttpRequest();
     req.open("GET", url, false);
+    //req.open("GET", url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), false); //Bypass cache
     req.send(null);
     return (req.status == 200) ? req.responseText : null;
 }
@@ -87,25 +116,25 @@ function loadShader(src, type) {
 }
 
 function makeShader() {
-    var vertSource = reqSource("shaders/blob.vert");
-    var fragSource = reqSource("shaders/blob.frag");
+    var vertSource = reqSource("/shaders/blob.vert");
+    var fragSource = reqSource("/shaders/blob.frag");
     var fragmentShader = loadShader(fragSource, gl.FRAGMENT_SHADER);
     var vertexShader = loadShader(vertSource, gl.VERTEX_SHADER);
-    
+
     shader = gl.createProgram();
     gl.attachShader(shader, vertexShader);
     gl.attachShader(shader, fragmentShader);
     gl.linkProgram(shader);
-    
+
     if (!gl.getProgramParameter(shader, gl.LINK_STATUS)) {
         throw gl.getProgramInfoLog(shader);
     }
-    
+
     gl.useProgram(shader);
 
     shader.vertexPositionAttribute = gl.getAttribLocation(shader, "in_pos");
     gl.enableVertexAttribArray(shader.vertexPositionAttribute);
-    
+
     shader.uniformTimeLocation = gl.getUniformLocation(shader, "u_time");
     shader.uniformAspectRatioLocation = gl.getUniformLocation(shader, "u_aspect_ratio");
 }
@@ -122,7 +151,7 @@ function makeVBO() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     fsqBuffer.itemSize = 2;
     fsqBuffer.numItems = 4;
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, fsqBuffer);
     gl.vertexAttribPointer(shader.vertexPositionAttribute, fsqBuffer.itemSize, gl.FLOAT, false, 0, 0);
 }
@@ -135,15 +164,15 @@ function animate() {
         startTime = currentTime;
     }
     var time = (currentTime - startTime)/1000.0;
-    
+
     try {
         gl.viewport(0, 0, width, height);
         gl.clear(gl.COLOR_BUFFER_BIT);
-        
+
         gl.uniform1f(shader.uniformTimeLocation, time);
         gl.uniform1f(shader.uniformAspectRatioLocation, aspectRatio);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, fsqBuffer.numItems);
-        
+
         window.requestAnimationFrame(animate);
     }
     catch (e) {
@@ -156,17 +185,18 @@ function main() {
         canvas = $("#demo");
         onResize();
         addEvent(window, "resize", onResize);
-        
+
         gl = canvas[0].getContext('experimental-webgl');
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
-        
+
+        makeCubemap();
         makeShader();
         makeVBO();
-        
+
         canvas.fadeIn("slow", function() {
             $("#spinner").fadeOut("fast");
         });
-        
+
         window.requestAnimationFrame(animate);
     }
     catch (e) {
